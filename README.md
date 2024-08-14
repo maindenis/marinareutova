cbb744bcae876acead70512ad2cb2e3129976cb8
maindenis <165322876+maindenis@users.noreply.github.com>
2024-08-14 13:24:55 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# https://maindenis.github.io/marinareutova/index.html
---
+++ js/scripts.js
@@ -0,0 +1,109 @@
+function getRespParams() {
+    // if(bodyWidth <= 768) {
+        if($(document).scrollTop() > 100) {
+            $(".header_site").addClass("scroll");
+        } else {
+            $(".header_site").removeClass("scroll");
+        }
+    // }
+}
+
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+$(window).resize(function() {
+    getRespParams();
+    bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+});
+
+$(document).scroll(function() {
+    getRespParams();
+});
+
+$(document).ready(function() {
+
+    getRespParams();
+
+    if( $(".slider").length > 0 ) {
+        $(".slider").not(".slick-initialized").slick({
+            dots: true,
+            arrows: true,
+            // autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 2000,
+            variableWidth: true,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            centerMode: true,
+            appendDots: $(".slider_dots"),
+            appendArrows: $(".slider_arrows"),
+            // fade: true,
+            responsive: [
+                {
+                  breakpoint: 1125,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                  }
+                },
+                // {
+                //   breakpoint: 540,
+                //   settings: {
+                //     slidesToShow: 1,
+                //     slidesToScroll: 1
+                //   }
+                // }
+              ]
+        });
+    }
+
+    // --------------
+
+    $(".respmenubtn").click(function(e) {
+      e.preventDefault();
+      if( $("#resp_nav").is(":hidden") ) {
+          $("#resp_nav").fadeIn(300);
+          $(this).addClass("active");
+      } else {
+          $("#resp_nav").fadeOut(300);
+          $(this).removeClass("active");
+      }
+    });
+    
+    $(this).keydown(function(eventObject){
+        if (eventObject.which == 27 &&
+            $("#resp_nav").is(":visible") &&
+            bodyWidth <= 767) {
+                $("#resp_nav").fadeOut(300);
+                $(".respmenubtn").removeClass("active");
+        }
+    });
+
+    // --------------
+
+    $(".dr").each(function() {
+      drContent = $(this).find(".dr_content");
+      if($(this).hasClass("active")) {      
+        drContent.slideDown(300);
+      } else {
+        drContent.slideUp(300);
+      }
+    });
+
+    $(".dr_title").on("click", function(e) {
+      e.preventDefault();
+      parent = $(this).closest(".dr");
+      sl = parent.find(".dr_content");
+      if(sl.is(":hidden")) {
+        parent.addClass("active");
+        sl.slideDown(300);
+      } else {
+        parent.removeClass("active");
+        sl.slideUp(300);
+      }
+    });
+
+});
